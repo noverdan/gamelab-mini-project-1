d971009d8a08a62e60d619b7c0db017205701bb0
Yusuf0803 <baeyusuf094@gmail.com>
2024-03-28 17:01:13 +0700

    Fix Bug

--- JS/Detail_Produk.js
+++ JS/Detail_Produk.js
@@ -10,93 +10,62 @@ fetch("https://65ffbff5df565f1a614547df.mockapi.io/products")
       const productName = document.createElement("h2");
       productName.textContent = product.title;
       productName.classList.add("product-name");
-
-      const productImages = product.image;
+      productCard.appendChild(productName);
 
       const imageContainer = document.createElement("div");
-      imageContainer.classList.add("swiper-container");
-
+      imageContainer.classList.add("swiper-container", "product-images-swiper");
       const swiperWrapper = document.createElement("div");
       swiperWrapper.classList.add("swiper-wrapper");
+      imageContainer.appendChild(swiperWrapper);
 
-      productImages.forEach((imageUrl, index) => {
+      const swiperButtonPrev = document.createElement("div");
+      swiperButtonPrev.classList.add("swiper-button-prev");
+      imageContainer.appendChild(swiperButtonPrev);
+
+      const swiperButtonNext = document.createElement("div");
+      swiperButtonNext.classList.add("swiper-button-next");
+      imageContainer.appendChild(swiperButtonNext);
+
+      productCard.appendChild(imageContainer);
+
+      const productDetails = document.createElement("div");
+      productDetails.classList.add("product-details");
+      productDetails.innerHTML = `<p>${product.id}</p><p>Rp. ${product.price}</p>`;
+      productCard.appendChild(productDetails);
+
+      const productDescription = document.createElement("p");
+      productDescription.innerHTML = product.description;
+      productDescription.classList.add("product-description");
+      productCard.appendChild(productDescription);
+
+      const buyButton = document.createElement("button");
+      buyButton.textContent = "Beli";
+      buyButton.classList.add("buy-button");
+      productCard.appendChild(buyButton);
+
+      productContainer.appendChild(productCard);
+
+      const swiper = new Swiper(imageContainer, {
+        direction: "horizontal",
+        loop: false,
+        slidesPerView: 1,
+        navigation: {
+          nextEl: swiperButtonNext,
+          prevEl: swiperButtonPrev,
+        },
+      });
+
+      product.image.forEach((imageUrl) => {
         const swiperSlide = document.createElement("div");
         swiperSlide.classList.add("swiper-slide");
 
         const productImage = document.createElement("img");
         productImage.src = imageUrl;
         productImage.alt = product.title;
-        productImage.style.display = index === 0 ? "block" : "none";
 
         swiperSlide.appendChild(productImage);
         swiperWrapper.appendChild(swiperSlide);
       });
-
-      imageContainer.appendChild(swiperWrapper);
-
-      const swiperButtonPrev = document.createElement("div");
-      swiperButtonPrev.classList.add("swiper-button-prev");
-
-      const swiperButtonNext = document.createElement("div");
-      swiperButtonNext.classList.add("swiper-button-next");
-
-      const productId = document.createElement("p");
-      productId.textContent = `${product.id}`;
-
-      const productPrice = document.createElement("p");
-      productPrice.textContent = `Rp. ${product.price}`;
-
-      const productDetails = document.createElement("div");
-      productDetails.classList.add("product-details");
-      productDetails.appendChild(productId);
-      productDetails.appendChild(productPrice);
-
-      const productDescription = document.createElement("p");
-      productDescription.innerHTML = product.description;
-
-      const buyButton = document.createElement('button');
-      buyButton.textContent = 'Beli';
-      buyButton.classList.add('buy-button');
-      buyButton.addEventListener('click', () => {
-          window.location.href = '../index.html';
-      });
-
-      productCard.appendChild(productName);
-      productCard.appendChild(imageContainer);
-      productCard.appendChild(productDetails);
-      productCard.appendChild(productDescription);
-      productCard.appendChild(swiperButtonPrev);
-      productCard.appendChild(swiperButtonNext);
-      productCard.appendChild(buyButton);
-
-      productContainer.appendChild(productCard);
-
-      const swiper = new Swiper(imageContainer, {
-        
-        direction: "horizontal",
-        loop: false,
-
-        slidesPerView: 1,
-        
-        navigation: {
-          nextEl: swiperButtonNext,
-          prevEl: swiperButtonPrev,
-        },
-
-        on: {
-          slideChange: function () {
-            const currentIndex = this.activeIndex;
-            const slides = this.slides;
-
-            slides.forEach((slide, index) => {
-              slide.querySelector("img").style.display =
-                index === currentIndex ? "block" : "none";
-            });
-          },
-        },
-      });
     });
   })
   .catch((error) => console.error("Error fetching data:", error));
-  
-
